eccc9106361ae9eebb545ba86fde926525dd5b21
bojan <bojankrsmanovic480@gmail.com>
2017-05-24 20:09:12 +0200

    Create ripple.js

---
+++ ripple.js
@@ -0,0 +1,62 @@
+(function () {
+    var isMobile = window.navigator.userAgent.match(/Mobile/) && window.navigator.userAgent.match(/Mobile/)[0] === "Mobile",
+    event = isMobile ? "touchend" : "click",
+    button = document.querySelectorAll('*[data-animation="ripple"]');
+    //Speed in miliseconds
+    var speed = 700;
+    var color = 'rgba(255,255,255,0.8)';
+
+    for (var i = 0; i < button.length; i++) {
+        var currentBtn = button[i];
+        // console.log(window.getComputedStyle(button[i], null).getPropertyValue('background'));
+
+        currentBtn.addEventListener(event, function(e) {
+            e.preventDefault();
+            const button = e.target,
+            rect = button.getBoundingClientRect(),
+            originalBtn = this,
+            btnHeight = rect.height,
+            btnWidth = rect.width;
+            let posMouseX = 0,
+            posMouseY = 0;
+
+            if (isMobile) {
+                posMouseX = e.changedTouches[0].pageX - rect.left;
+                posMouseY = e.changedTouches[0].pageY - rect.top;
+            } else {
+                posMouseX = e.pageX - rect.left;
+                posMouseY = e.pageY - rect.top;
+            }
+
+            var baseCSS =  `position: absolute;
+            width: ${btnWidth * 2}px;
+            height: ${btnWidth * 2}px;
+            transition: all linear ` + speed + `ms;
+            transition-timing-function:cubic-bezier(0.250, 0.460, 0.450, 0.940);
+            border-radius: 50%;
+            background: ` + color + `;
+            top:${posMouseY - btnWidth}px;
+            left:${posMouseX - btnWidth}px;
+            pointer-events: none;
+            transform:scale(0)`
+
+            var ripple = document.createElement("span");
+            ripple.style.cssText = baseCSS;
+
+            currentBtn.style.overflow = "hidden";
+            this.appendChild(ripple);
+
+            //start animation
+            setTimeout( function() {
+                ripple.style.cssText = baseCSS + `transform:scale(1); opacity: 0;`;
+            }, 5);
+
+            setTimeout( function() {
+                ripple.remove();
+                if (currentBtn.href) {
+                    window.location.href = currentBtn.href;
+                }
+            }, speed);
+        })
+    }
+}());
