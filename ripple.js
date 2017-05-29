edb56fb1a02726a60f979ac46a8c536cd0b67912
Mladen Ilic <mladen@stuntcoders.com>
2017-05-29 20:40:48 +0200

    Improve code style

--- ripple.js
+++ ripple.js
@@ -1,20 +1,22 @@
 (function () {
-    var isMobile = window.navigator.userAgent.match(/Mobile/) && window.navigator.userAgent.match(/Mobile/)[0] === "Mobile",
-    event = isMobile ? "touchend" : "click",
-    button = document.querySelectorAll('*[data-animation="ripple"]');
+    var isMobile = window.navigator.userAgent.match(/Mobile/) && window.navigator.userAgent.match(/Mobile/)[0] === "Mobile";
+    var event = isMobile ? 'touchend' : 'click';
+    var button = document.querySelectorAll('*[data-animation="ripple"]');
 
     for (var i = 0; i < button.length; i++) {
         var currentBtn = button[i];
 
-        currentBtn.addEventListener(event, function(e) {
+        currentBtn.addEventListener(event, function (e) {
             e.preventDefault();
-            const button = e.target,
-            rect = button.getBoundingClientRect(),
-            originalBtn = this,
-            btnHeight = rect.height,
-            btnWidth = rect.width;
-            let posMouseX = 0,
-            posMouseY = 0;
+            var button = e.target;
+
+            var rect = button.getBoundingClientRect();
+            var originalBtn = this;
+            var btnHeight = rect.height;
+            var btnWidth = rect.width;
+
+            var posMouseX = 0;
+            var posMouseY = 0;
 
             if (isMobile) {
                 posMouseX = e.changedTouches[0].pageX - rect.left;
@@ -24,22 +26,23 @@
                 posMouseY = e.pageY - rect.top;
             }
 
-            var speed = this.dataset.speed || 700,
-            color = this.dataset.color || 'rgba(255,255,255,0.8)';
+            var speed = this.dataset.speed || 700;
+            var color = this.dataset.color || 'rgba(255, 255, 255, 0.8)';
 
             var baseCSS =  `position: absolute;
-            width: ${btnWidth * 2}px;
-            height: ${btnWidth * 2}px;
-            transition: all linear ` + speed + `ms;
-            transition-timing-function:cubic-bezier(0.250, 0.460, 0.450, 0.940);
-            border-radius: 50%;
-            background: ` + color + `;
-            top:${posMouseY - btnWidth}px;
-            left:${posMouseX - btnWidth}px;
-            pointer-events: none;
-            transform:scale(0)`
+                width: ${btnWidth * 2}px;
+                height: ${btnWidth * 2}px;
+                transition: all linear ` + speed + `ms;
+                transition-timing-function:cubic-bezier(0.250, 0.460, 0.450, 0.940);
+                border-radius: 50%;
+                background: ` + color + `;
+                top: ${posMouseY - btnWidth}px;
+                left:${posMouseX - btnWidth}px;
+                pointer-events: none;
+                transform:scale(0)`;
 
             var ripple = document.createElement("span");
+            
             ripple.style.cssText = baseCSS;
 
             this.style.position = "relative";
